5ffc484de412ba1f4bd4ebbba6c5da9f8b25544e
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-04-04 13:29:09 +0530

    first commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -10,7 +10,7 @@ const Home = (props) => {
   return (
     <div>
       {/* Home Section Background */}
-      <div id="home" className="h-[100vh] p-4 pb-20">
+      <div id="home" className="h-[80vh] p-4">
         {/* Floating Card */}
         <div className="bg-white w-[80%] mx-auto -mt-100 rounded-lg shadow-2xl relative z-10">
           <div className="flex flex-col md:flex-row items-center p-6">
