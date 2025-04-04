e75d4a97180942c1d6e8394d1c36478995c1baa0
SaiManikya Mallikharjun Khandavilli <saimallikharjun81@gmail.com>
2025-04-04 12:47:52 +0530

    first commit

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -21,7 +21,7 @@ const Home = (props) => {
             <div className="flex-1">
               <p className="text-sm pt-5 pl-5">HELLO EVERYBODY, I AM</p>
               <h1 className="text-2xl pl-5">SAI MANIKYA MALLIKHARJUN</h1>
-              <p className="text-sm pt-2 px-5 text-gray-500 font-bold">
+              <p className="text-xl pt-2 px-5 text-white font-bold">
                 A passionate frontend developer with 4+ years of experience
                 building responsive, user-friendly web applications. Skilled in
                 React.js, JavaScript, Tailwind CSS, Bootstrap, CSS and HTML. I
@@ -30,8 +30,8 @@ const Home = (props) => {
               </p>
 
               <div className="flex flex-col justify-center gap-10 pl-5 pt-1">
-                <i className="bi bi-house text-2xl text-[#8da7e4]"></i>
-                <div className="text-2xl text-gray-500">
+                <i className="bi bi-house text-2xl text-white"></i>
+                <div className="text-2xl text-white">
                   <LocationComponent />
                 </div>
               </div>
@@ -41,33 +41,33 @@ const Home = (props) => {
                   href="https://github.com/SaiMallikharjun98"
                   target="_blank"
                   rel="noreferrer"
-                  className="hover:text-[#948ee6]"
+                  className="hover:text-blue-500 "
                   aria-label="GitHub"
                 >
-                  <i className="bi bi-github text-[rgb(173,173,237)] text-2xl"></i>
+                  <i className="bi bi-github text-white text-2xl"></i>
                 </a>
                 <a
                   href="https://www.linkedin.com/in/smmallikharjunkhandavilli"
                   target="_blank"
                   rel="noreferrer"
-                  className="hover:text-[#948ee6]"
+                  className="hover:text-blue-500"
                   aria-label="LinkedIn"
                 >
-                  <i className="bi bi-linkedin text-[rgb(173,173,237)] text-2xl"></i>
+                  <i className="bi bi-linkedin text-white text-2xl"></i>
                 </a>
                 <a
                   href="mailto:mallikharjun7965@gmail.com"
-                  className="hover:text-[#948ee6]"
+                  className="hover:text-blue-500"
                   aria-label="Email"
                 >
-                  <i className="bi bi-envelope text-[rgb(173,173,237)] text-2xl"></i>
+                  <i className="bi bi-envelope text-white text-2xl"></i>
                 </a>
                 <a
                   href="tel:+91 8919842383"
-                  className="hover:text-[#948ee6]"
+                  className="hover:text-blue-500"
                   aria-label="Phone"
                 >
-                  <i className="bi bi-phone text-[rgb(173,173,237)] text-2xl"></i>
+                  <i className="bi bi-phone text-white text-2xl"></i>
                 </a>
               </div>
             </div>
